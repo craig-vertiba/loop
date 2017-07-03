71d0891e0965b3c3697316bd66442e4f7ad019bb
craig-vertiba <craig-vertiba@users.noreply.github.com>
2017-07-03 06:33:22 -0600

    Update custom.js

--- custom.js
+++ custom.js
@@ -1,6 +1,6 @@
 // GoogleMaps function:
 function initMap() {
-  var uluru = {lat: -25.363, lng: 131.044};
+  var uluru = {lat: 40.015, lng: -105.271};
   var map = new google.maps.Map(document.getElementById('map'), {
     zoom: 4,
     center: uluru
