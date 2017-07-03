7134d9d83b0ab69444f6a4d49b066915bd96663e
craig-vertiba <craig-vertiba@users.noreply.github.com>
2017-07-03 08:15:25 -0600

    Update nroll-script.js

--- nroll-script.js
+++ nroll-script.js
@@ -368,25 +368,6 @@
             // This is the id value of the div to which the entire plugin will be appended.
             var div = $("#nroll-plugin");
             div.load(base_url+'content.html');
-            var surveyJSON = { title: "Tell us, what technologies do you use?", pages: [
-              { name:"page1", questions: [ 
-                  { type: "radiogroup", choices: [ "Yes", "No" ], isRequired: true, name: "frameworkUsing",title: "Do you use any front-end framework like Bootstrap?" },
-                  { type: "checkbox", choices: ["Bootstrap","Foundation"], hasOther: true, isRequired: true, name: "framework", title: "What front-end framework do you use?", visibleIf: "{frameworkUsing} = 'Yes'" }
-               ]},
-              { name: "page2", questions: [
-                { type: "radiogroup", choices: ["Yes","No"],isRequired: true, name: "mvvmUsing", title: "Do you use any MVVM framework?" },
-                { type: "checkbox", choices: [ "AngularJS", "KnockoutJS", "React" ], hasOther: true, isRequired: true, name: "mvvm", title: "What MVVM framework do you use?", visibleIf: "{mvvmUsing} = 'Yes'" } ] },
-              { name: "page3",questions: [
-                { type: "comment", name: "about", title: "Please tell us about your main requirements for Survey library" } ] }
-             ]
-            };
-            Survey.Survey.cssType = "bootstrap";
-            var survey = new Survey.Model(surveyJSON);
-            $("#surveyElement").SurveyWindow({
-                model:survey
-            });
-
-
 
        }); // end jquery.documentready
 
