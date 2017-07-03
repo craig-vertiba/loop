180e136b6d8601bf25e51c9d93fd918b3a537f91
craig-vertiba <craig-vertiba@users.noreply.github.com>
2017-07-03 07:05:02 -0600

    Update custom.js

--- custom.js
+++ custom.js
@@ -10,17 +10,3 @@ function initMap() {
     map: map
   });
 }
-
-// Custom JS from SurveyJS
-Survey.Survey.cssType = "bootstrap";
-Survey.defaultBootstrapCss.navigationButton = "btn btn-green";
-
-window.survey = new Survey.Model( { questions: [
-     {name:"name", type:"text", title: "Please enter your name:", placeHolder:"Jon Snow", isRequired: true},
-     {name:"birthdate", type:"text", inputType:"date", title: "Your birthdate:", isRequired: true},
-     {name:"color", type:"text", inputType:"color", title: "Your favorite color:"},
-     {name:"email", type:"text", inputType:"email", title: "Your e-mail:", placeHolder:"jon.snow@nightwatch.org", isRequired: true, validators: [{type:"email"}]}
-]});
-survey.onComplete.add(function(result) {
-	document.querySelector('#surveyResult').innerHTML = "result: " + JSON.stringify(result.data);
-});
