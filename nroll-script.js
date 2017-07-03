ca049ac13385bf5a4a3769e8847ea916f14f4be2
craig-vertiba <craig-vertiba@users.noreply.github.com>
2017-07-03 06:59:38 -0600

    Update nroll-script.js

--- nroll-script.js
+++ nroll-script.js
@@ -365,6 +365,7 @@
             // This is the id value of the div to which the entire plugin will be appended.
             var div = $("#nroll-plugin");
             div.load(base_url+'content.html');
+            $("#surveyElement").Survey({model:survey});
 
        }); // end jquery.documentready
 
