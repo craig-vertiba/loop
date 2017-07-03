18bc9a42f7d6533d6c8bdd2d218c46f476a4097c
craig-vertiba <craig-vertiba@users.noreply.github.com>
2017-07-03 06:14:10 -0600

    Update custom.js

--- custom.js
+++ custom.js
@@ -1,14 +1,13 @@
 // GoogleMaps function:
-<script>
-  function initMap() {
-    var uluru = {lat: -25.363, lng: 131.044};
-    var map = new google.maps.Map(document.getElementById('map'), {
-      zoom: 4,
-      center: uluru
-    });
-    var marker = new google.maps.Marker({
-      position: uluru,
-      map: map
-    });
-  }
-</script>
+function initMap() {
+  var uluru = {lat: -25.363, lng: 131.044};
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 4,
+    center: uluru
+  });
+  var marker = new google.maps.Marker({
+    position: uluru,
+    map: map
+  });
+}
+
