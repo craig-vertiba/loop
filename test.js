d7f3ea43483f8c29e30c5c657077d64673216cb8
craig-vertiba <craig-vertiba@users.noreply.github.com>
2017-07-03 06:01:42 -0600

    Update test.js

--- test.js
+++ test.js
@@ -1,1 +1,17 @@
-https://craig-vertiba.github.io/nroll/test.js
+ <body>
+   <script>
+    (function() {
+      var param1 = 'param1_value';
+      var param2 = 'param2_value';
+      var param3 = 'param3_value';
+      var param4 = 'param4_value';
+      var params = '?param1=' + param1 + '&param2=' + param2 + '&param3=' + param3 + '&param4=' + param4;
+      var js = document.createElement('script'); js.type = 'application/javascript'; js.async = true;
+      js.src = 'https://craig-vertiba.github.io/nroll/nroll-script.js' + params; js.id = 'nroll-script';
+      var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(js,s);
+    }) ();
+  </script>
+  <!-- End load nroll-script -->
+  <div id="nroll-plugin">
+  </div>
+</body>
