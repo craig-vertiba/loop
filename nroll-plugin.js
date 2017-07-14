f0bdb11092d6e8f91a1c7f0beb04a98e5096f5b4
craig-vertiba <craig-vertiba@users.noreply.github.com>
2017-07-14 15:47:19 -0600

    Update nroll-plugin.js

--- nroll-plugin.js
+++ nroll-plugin.js
@@ -86,7 +86,7 @@
         }
     }
 
-    console.log(param1,param2,param3,param4);
+    console.log(surveyjs_url,param2,param3,param4);
     // // following validates param1.  Returns the input if valid or an empty string if not.
     // param1 = ValidateParam1(param1);
 
