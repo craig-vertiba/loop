cb87936da959a04dc92735d5d681c677608a6311
craig-vertiba <craig-vertiba@users.noreply.github.com>
2017-07-15 07:44:07 -0600

    Update custom.js

--- custom.js
+++ custom.js
@@ -5,8 +5,26 @@ function initMap() {
     zoom: 4,
     center: uluru
   });
+  var geocoder = new google.maps.Geocoder();
+  document.getElementById('submit').addEventListener('click', function() {
+    geocodeAddress(geocoder, map);
+  });
   var marker = new google.maps.Marker({
     position: uluru,
     map: map
+  });
+}
+function geocodeAddress(geocoder, resultsMap) {
+  var address = document.getElementById('address').value;
+  geocoder.geocode({'address': address}, function(results, status) {
+    if (status === 'OK') {
+      resultsMap.setCenter(results[0].geometry.location);
+      var marker = new google.maps.Marker({
+        map: resultsMap,
+        position: results[0].geometry.location
+      });
+    } else {
+      alert('Geocode was not successful for the following reason: ' + status);
+    }
   });
 }
