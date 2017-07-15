0b6bbbc64d237f24a7e44e747e6e86aed5d0ef1d
craig-vertiba <craig-vertiba@users.noreply.github.com>
2017-07-15 08:44:18 -0600

    Update custom.js

--- custom.js
+++ custom.js
@@ -1,10 +1,18 @@
 // GoogleMaps function:
 function initMap() {
+  var locations = [
+     ['Title A', 40.025,-105.261, 1],
+     ['Title B', 40.035,-105.251, 2],
+     ['Title C', 40.045,-105.241, 3],
+     ['Title D', 40.055,-105.231, 4]
+  ];
   var uluru = {lat: 40.015, lng: -105.271};
   var map = new google.maps.Map(document.getElementById('map'), {
     zoom: 4,
     center: uluru
   });
+  var infowindow = new google.maps.InfoWindow;
+  var marker, i;
   var geocoder = new google.maps.Geocoder();
   document.getElementById('submit').addEventListener('click', function() {
     geocodeAddress(geocoder, map);
@@ -13,6 +21,19 @@ function initMap() {
     position: uluru,
     map: map
   });
+  for (i = 0; i < locations.length; i++) {  
+    marker = new google.maps.Marker({
+         position: new google.maps.LatLng(locations[i][1], locations[i][2]),
+         map: map
+    });
+
+    google.maps.event.addListener(marker, 'click', (function(marker, i) {
+         return function() {
+             infowindow.setContent(locations[i][0]);
+             infowindow.open(map, marker);
+         }
+    })(marker, i));
+  }
 }
 function geocodeAddress(geocoder, resultsMap) {
   var address = document.getElementById('address').value;
