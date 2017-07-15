54166ee563143dd01c8964a7c9823a9aab123140
craig-vertiba <craig-vertiba@users.noreply.github.com>
2017-07-15 12:52:39 -0600

    Update nroll-plugin.js

--- nroll-plugin.js
+++ nroll-plugin.js
@@ -333,7 +333,7 @@
                 survey.onComplete.add(function(result) {
                     // document.querySelector('#surveyResult').innerHTML = "result: " + JSON.stringify(result.data);
                     $("#surveyElement").css('display', 'none');
-                    $("#plugin-map").css('display', 'inline-block');
+                    $("#plugin-map").css('display', 'inline');
                 });
                 $("#surveyElement").Survey({
                     model: survey,
