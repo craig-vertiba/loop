6ebfa7527ec9ca91d3bdda48394d0c3181537fab
craig-vertiba <craig-vertiba@users.noreply.github.com>
2017-07-22 07:54:53 -0500

    Update nroll-plugin.js

--- nroll-plugin.js
+++ nroll-plugin.js
@@ -562,7 +562,7 @@
                     locations[i][4] = google.maps.geometry.spherical.computeDistanceBetween(results[0].geometry.location, this_location);  
                     console.log(locations[i][0],locations[i][1],locations[i][2],locations[i][3],locations[i][4]);
                 }
-                locations[4].sort(function(a, b){return a-b});
+                locations.sort(function(a, b){return a[4]-b[4]});
                 for (i = 0; i < locations.length; i++) {
                     console.log(locations[i][0],locations[i][1],locations[i][2],locations[i][3],locations[i][4]);
                 }
