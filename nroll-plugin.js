45bc98f4982d87f427e52edf455a768e6b5a375e
craig-vertiba <craig-vertiba@users.noreply.github.com>
2017-08-05 15:57:46 -0600

    Update nroll-plugin.js

--- nroll-plugin.js
+++ nroll-plugin.js
@@ -582,17 +582,17 @@
         })(marker, i));
       }
     }
-    function siteSelected(resultsMap,clicked_id,locations) {
+    function siteSelected(resultsMap,clicked_id) {
         // get the page element to which we need to add the selected site
         var d1 = document.getElementById('selected-site');
         // delete any html already attached to that element (like a previously selected site)
         d1.innerHTML="";
         // add new html to display the selected site
-        var index = clicked_id.slice(9);
-        var a = index + 1;
+        var i = Number(clicked_id.slice(9));
+        var a = i + 1;
         d1.insertAdjacentHTML('beforeend', '<hr/><div><div style="width:20%;float:left;min-height:1px">'+a+'</div><div style="width:60%;display:inline-block;text-align:left">'+locations[i].name+'</div><div style="width:20%;display:inline-block;min-height:1px;text-align:bottom-right"><button id="change-location">Change</button></div></div>');
 
-        console.log('in siteSelected'+clicked_id.slice(9));
+        console.log('in siteSelected'+i);
     }
     function geocodeAddress(geocoder, resultsMap) {
         var address = document.getElementById('address').value;
@@ -633,7 +633,7 @@
                     d1.insertAdjacentHTML('beforeend', '<hr/><div><div style="width:20%;float:left;min-height:1px">'+a+'</div><div style="width:60%;display:inline-block;text-align:left">'+locations[i].name+'<br/>'+locations[i].street+'<br/>'+locations[i].city+'<br/>'+locations[i].state+', '+locations[i].zip+'</div><div style="width:20%;display:inline-block;min-height:1px;text-align:bottom-right"><button id="location-'+i+'" class="site-selector">Select</button></div></div>');
                     console.log(locations[i].name,locations[i].lat,locations[i].long,locations[i].order,locations[i].distance);
                     document.getElementById('location-' + i).addEventListener('click', function() {
-                        siteSelected(resultsMap,this.id,locations);
+                        siteSelected(resultsMap,this.id);
                     });
                 }
                 // clear all existing site location markers from the map
