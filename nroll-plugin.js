3c21aa1e881d5faf519b596989b41541b365113f
craig-vertiba <craig-vertiba@users.noreply.github.com>
2017-08-05 12:48:27 -0600

    Update nroll-plugin.js

--- nroll-plugin.js
+++ nroll-plugin.js
@@ -582,8 +582,15 @@
         })(marker, i));
       }
     }
-    function siteSelected(resultsMap) {
-        console.log('in siteSelected');
+    function siteSelected(resultsMap,clicked_id) {
+        // get the page element to which we need to add the selected site
+        //var d1 = document.getElementById('selected-site');
+        // delete any html already attached to that element (like a previously selected site)
+        //d1.innerHTML="";
+        // add new html to display the selected site
+        //d1.insertAdjacentHTML('beforeend', '<hr/><div><div style="width:20%;float:left;min-height:1px">'+a+'</div><div style="width:60%;display:inline-block;text-align:left">'+locations[i].name+'<br/>'+locations[i].street+'<br/>'+locations[i].city+'<br/>'+locations[i].state+', '+locations[i].zip+'</div><div style="width:20%;display:inline-block;min-height:1px;text-align:bottom-right"><button id="location-'+i+'" class="site-selector">Select</button></div></div>');
+
+        console.log('in siteSelected'+clicked_id);
     }
     function geocodeAddress(geocoder, resultsMap) {
         var address = document.getElementById('address').value;
@@ -624,14 +631,14 @@
                     d1.insertAdjacentHTML('beforeend', '<hr/><div><div style="width:20%;float:left;min-height:1px">'+a+'</div><div style="width:60%;display:inline-block;text-align:left">'+locations[i].name+'<br/>'+locations[i].street+'<br/>'+locations[i].city+'<br/>'+locations[i].state+', '+locations[i].zip+'</div><div style="width:20%;display:inline-block;min-height:1px;text-align:bottom-right"><button id="location-'+i+'" class="site-selector">Select</button></div></div>');
                     console.log(locations[i].name,locations[i].lat,locations[i].long,locations[i].order,locations[i].distance);
                     document.getElementById('location-' + i).addEventListener('click', function() {
-                        siteSelected(resultsMap);
+                        siteSelected(resultsMap,this.id);
                     });
                 }
                 // clear all existing site location markers from the map
                 for (i = 0; i < marker.length; i++) {
                     marker[i].setMap(null);
                 }
-                // reinitialize the marker array
+                // empty the marker array
                 markers = [];
                 // loop through all the locations and create new markers numbered in ascending order based on the site's distance 
                 // from the user's new location, then add an infowindow listener to each marker
