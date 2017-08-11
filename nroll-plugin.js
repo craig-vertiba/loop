568b1afa1c6fe5d6cab563b8ca0a917f356deb1e
craig-vertiba <craig-vertiba@users.noreply.github.com>
2017-08-11 07:46:16 -0600

    Update nroll-plugin.js

--- nroll-plugin.js
+++ nroll-plugin.js
@@ -377,9 +377,9 @@
                 return $.ajax({
                     type:'POST',
                     url: api_base_url + "ApplicationPlugin",
-                    // headers:{'Authorization':'Bearer ' + access_token,
-                    //          'Content-Type': 'application/json'},
-                    headers:{'Authorization':'Bearer ' + access_token},
+                    headers:{'Authorization':'Bearer ' + access_token,
+                             'Content-Type': 'application/json'},
+                    //headers:{'Authorization':'Bearer ' + access_token},
                     data: new_application_data,
                     //jsonp: false,
                     //dataType: 'json',
